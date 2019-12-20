f9adfb7953dae8fbba68d36458c9bb954a9f84b9
unknown <abeliu@corp.ingatek.com>
2019-12-20 16:42:55 +0800

    4_flower improve version

--- samples/4_flower.py
+++ samples/4_flower.py
@@ -1,8 +1,11 @@
 for i in range(100,1000):
-    a = i%10
-    b = i//10%10
-    c = i//100
-    if pow(a,3)+pow(b,3)+pow(c,3)==i:
+    sum = 0
+    data = i
+    for j in range(3):
+        bits = data%10
+        data = data // 10
+        sum += pow(bits,3)
+    if sum==i:
         print(i,end=' ')
 print("")
 

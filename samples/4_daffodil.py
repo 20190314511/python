2013a0ce142ec1e94a9c671f5991d06543930be1
unknown <abeliu@corp.ingatek.com>
2019-12-24 13:36:53 +0800

    * improve python speed, import numba * add python vs c.md file

--- samples/4_daffodil.py
+++ samples/4_daffodil.py
@@ -1,33 +1,38 @@
+# tobe continued: speedup using numba
+from numba import jit
 import math
 import time
 import sys
 
-# 准备pow数组，p[i][j]保存pow(i,j)
-p = []
-item = []
-for i in range(10):
+@jit
+def jittest():
+    # 准备pow数组，p[i][j]保存pow(i,j)
+    p = []
     item = []
-    for j in range(10):
-        item.append( pow(i,j) )
-    p.append(item)
+    for i in range(10):
+        item = []
+        for j in range(10):
+            item.append( pow(i,j) )
+        p.append(item)
 
-start = time.time()
+    start = time.time()
 
-for bits_count in range(3,10):
-    loopstart = pow(10, bits_count-1)
-    loopend = pow(10, bits_count)
-    
-    for i in range(loopstart, loopend):
-        sum = 0
-        data = i
-        while(data>0):
-            bits = data%10
-            data = data // 10
-            sum += p[bits][bits_count]
-        if sum==i:
-            print(i,end=' ')
-            sys.stdout.flush()
-    end = time.time()
-    print("\ttime used:%s"%(end-start))
+    for bits_count in range(3,10):
+        loopstart = pow(10, bits_count-1)
+        loopend = pow(10, bits_count)
+        
+        for i in range(loopstart, loopend):
+            sum = 0
+            data = i
+            while(data>0):
+                bits = data%10
+                data = data // 10
+                sum += p[bits][bits_count]
+            if sum==i:
+                print(i,end=' ')
+                sys.stdout.flush()
+        end = time.time()
+        print("\ttime used:%s"%(end-start))
 
+jittest()
 

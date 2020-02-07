bdae112cdb55725f03553a1e645372afae2dcc06
abeliu <abeliu@t3600>
2020-02-07 17:02:06 +0800

    add turtle.py

--- 1_Overview/hello.py
+++ 1_Overview/hello.py
@@ -1,5 +1,6 @@
 # Tim Peter  Zen of Python
 import this
+import math
 
 # single line comment 汉字测试
 
@@ -22,4 +23,14 @@ pie = 3.14159
 r = float(input("r="))
 print( 's=%.2f, l=%.2f' % (r*r*pie, 2*pie*r) )
 
-
+# 循环及换行测试
+for i in range(10):
+    print(i,end="")
+    if (i+1)%4==0:
+        print()
+        
+# 循环
+for i in range(100):
+    print("sqrt(%2d)=%5.3f"%(i,math.sqrt(i) ), end="  " )
+    if (i+1)%4==0:
+        print()

bc891f046fe3d9eb1e4f3b353699606cda840556
abeliu <abeliu@t3600>
2020-02-08 11:45:59 +0800

    add bulls&cows game v2

--- 1_Overview/bullsAcowsV2.py
+++ 1_Overview/bullsAcowsV2.py
@@ -1,11 +1,48 @@
 import random as r
 import time
 
+def judge(randstr, guessstr):
+    rbits=[]
+    gbits=[]
+    A=0
+    B=0
+
+    # 将字符串拆为单字符数组，便于后续比较
+    for c in randstr:
+        rbits.append(c)
+    for c in guessstr:
+        gbits.append(c)
+
+    right_char = "?"
+    # 处理猜对位数，并将各位清孔
+    for i in range( len(gbits) ):
+        if gbits[i]==rbits[i]:
+            A+=1
+            gbits[i]=right_char
+            rbits[i]=right_char
+
+    # 将猜对的位数去除
+    if A>0:
+        for i in range( gbits.count(right_char) ):
+            gbits.remove(right_char)
+            rbits.remove(right_char)        
+        randstr = ""
+        for j in range(len(rbits)):
+            randstr += rbits[j]
+
+    # 比较其余数字正确位数    
+    for c in gbits:
+        if c=="":
+            continue
+        if( randstr.rfind(c)>=0):
+            B+=1
+    return "A"+str(A)+"B"+str(B)  
+
 print("开始猜数")
 
 stopguess = False
 counter = 0
-timestart = time.process_time()
+timestart = time.process_time_ns()
 
 while stopguess==False:
     rand = r.randint(1000,10000)
@@ -13,30 +50,18 @@ while stopguess==False:
     while True:
         guess=int(input("请输入猜测的四位数："))
         counter += 1
-        timeend = time.process_time()
+        timeend = time.process_time_ns()
 
         if guess==rand:
-            print("共猜测%d次，用时%d秒。"%(counter, timeend-timestart))
+            print("共猜测%d次，用时%d秒。"%(counter, (timeend-timestart)/1000000))
             cont = input("继续猜测？").lower()
             if cont.rfind("no")>=0:
                 stopguess = True
             break
         else:
-            guessbk = guess
-            randbk = rand
-            A = 0
-            B = 0
-            for i in range(4):
-                gbits = guessbk%10
-                rbits = randbk%10
-                guessbk = guessbk/10
-                randbk = randbk/10
-                if (gbits==rbits):
-                    A+=1
-                else:
-                    if str(rand).rfind(str(gbits))>0:
-                        B+=1
-            print("results: %1dA%1dB"%(A,B))
+            guessbk = str(guess)
+            randbk = str(rand)
+            print(randbk+":"+judge(randbk, guessbk))
             continue
     
 
